adfef327d14ed156d32d86ee9d57dbb92eab89f4
mvitalia <p.guglielmo@mvitalia.com>
2016-11-24 16:26:15 +0100

    a

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var app = (function()
 		
 		
           cordova.plugins.BluetoothStatus.initPlugin();
-		  alert("ok");
+		  alert("init Bluetooth");
 		// Specify a shortcut for the location manager holding the iBeacon functions.
 		window.locationManager = cordova.plugins.locationManager;
 
