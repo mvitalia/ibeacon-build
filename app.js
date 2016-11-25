01444a581cb72e136f59c7660bc36e78cda604ca
mvitalia <p.guglielmo@mvitalia.com>
2016-11-25 17:38:48 +0100

    a

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ app.runScanTimer = function()
 			// Only show beacons that are updated during the last 60 seconds.
 			if (beacon.timeStamp + 60000 > timeNow)
 			{
-				alert=("ok");
+				alert("ok");
 				// Map the RSSI value to a width in percent for the indicator.
 				var rssiWidth = 1; // Used when RSSI is zero or greater.
 				if (beacon.rssi < -100) { rssiWidth = 100; }
