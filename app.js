9ee17a150eee1c4fb3db752570a810d1461e4885
mvitalia <p.guglielmo@mvitalia.com>
2016-11-28 10:35:31 +0100

    a

--- app.js
+++ app.js
@@ -146,12 +146,9 @@ app.runScanTimer = function()
 				// key, la chiave identifica
 				// Queto if permette di idetificare il Beacon a seconda della distanza
 				//prova = beacon.uuid;
-				if("a"=="a" && "b"=="b")
-				{
-					alert("Esegui");
-				}
 				if(countUno==0 && beacon.uuid=="5F4DF8FB-3EC2-60B1-DB6F-6E7013122EE0")
 				{
+					alert("entra");
 					//alert("UUID trovato: "+beacon.uuid);
 				//	alert("uno");
 					 navigator.notification.beep(1);
