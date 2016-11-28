ef24347388e497da54384c17511a33db45e710bf
mvitalia <p.guglielmo@mvitalia.com>
2016-11-28 15:06:10 +0100

    a

--- app.js
+++ app.js
@@ -190,8 +190,8 @@ app.runScanTimer = function()
 		{
 		
 			//alert('didStartMonitoringForRegion:' + JSON.stringify(pluginResult.state))
-			if (inBackground)
-		   	{
+			//if (inBackground)
+		   	//{
 				// Show notification if a beacon is inside the region.
 				// TODO: Add check for specific beacon(s) in your app.
 				if (pluginResult.region.typeName == 'BeaconRegion' &&
@@ -206,7 +206,7 @@ app.runScanTimer = function()
 						});
 						alert("DopoInvio");
 				}
-			}
+			//}
 			
 		};
 
