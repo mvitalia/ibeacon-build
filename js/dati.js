191b6658ba457a9917852f14683c24d96247e32f
mvitalia <p.guglielmo@mvitalia.com>
2016-11-29 09:48:08 +0100

    a

--- js/dati.js
+++ js/dati.js
@@ -87,8 +87,8 @@ function successoSelect(tx,dati)
              for(var i=0; i<len; i++)
             {
                
-                li_dati += "<li id="+dati.rows.item(i).id+" data-itemid="+dati.rows.item(i).id+"><a class='storage' href='#'><img src='"+dati.rows.item(i).uuid+"'/><h6 style='font-size:12px;'>" + dati.rows.item(i).titolo + " - " + dati.rows.item(i).link + "</h6>"
-                        +"<p style='font-size:10px;'>E-mail: "+dati.rows.item(i).uuid+"</p></a></li>"
+                li_dati += "<li id="+dati.rows.item(i).id+" data-itemid="+dati.rows.item(i).id+"><a class='storage' href='#'><img src='"+dati.rows.item(i).uuid+"'/><h6 style='font-size:12px;'>" + dati.rows.item(i).titolo + " - " + dati.rows.item(i).data_ora + "</h6>"
+                        +"<p style='font-size:10px;'>UUID: "+dati.rows.item(i).uuid+"</p></a></li>"
             
             }
             
