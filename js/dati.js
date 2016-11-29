6df7daa18e1595c3d500aa6304ea837308d14532
mvitalia <p.guglielmo@mvitalia.com>
2016-11-29 10:29:02 +0100

    a

--- js/dati.js
+++ js/dati.js
@@ -86,7 +86,8 @@ function successoSelect(tx,dati)
              
              for(var i=0; i<len; i++)
             {
-               alert(dati.rows.item(i).data_ora.getDay());
+                var giorno = dati.rows.item(i).data_ora.getDay();
+               alert(giorno);
                 li_dati += "<li id="+dati.rows.item(i).id+" data-itemid="+dati.rows.item(i).id+"><a class='storage' href='#'><img src='"+dati.rows.item(i).uuid+"'/><h6 style='font-size:12px;'>" + dati.rows.item(i).titolo + " - " + dati.rows.item(i).data_ora + "</h6>"
                         +"<p style='font-size:10px;'>UUID: "+dati.rows.item(i).uuid+"</p></a></li>"
             
