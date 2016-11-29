cd1e1bcf9323dd709d49cb4a969d09c946ae8e65
mvitalia <p.guglielmo@mvitalia.com>
2016-11-29 10:10:02 +0100

    a

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ var app = (function()
                             // Metodo di chiamata asincrona
                             function(tx) {
                                             //tx.executeSql("CREATE TABLE IF NOT EXISTS letture (id INTEGER PRIMARY KEY AUTOINCREMENT,uuid, major, minor, data_ora, proximity)");
-										    //tx.executeSql("DROP TABLE IF EXISTS notifiche");
-											tx.executeSql("CREATE TABLE IF NOT EXISTS notifiche (id INTEGER PRIMARY KEY AUTOINCREMENT,uuid, data_ora, titolo, descrizione, immagine, link, allegato, attivo_da, attivo_a)");
+										    tx.executeSql("DROP TABLE IF EXISTS notifiche");
+											//tx.executeSql("CREATE TABLE IF NOT EXISTS notifiche (id INTEGER PRIMARY KEY AUTOINCREMENT,uuid, data_ora text, titolo, descrizione, immagine, link, allegato, attivo_da, attivo_a)");
                                           },
                              function () {
                                              alert("Errore"+e.message);
@@ -174,9 +174,9 @@ app.runScanTimer = function()
   					aaaa = data.getFullYear();
   					Hh = data.getHours() + ":";
   					Mm = data.getMinutes() + ":";
-  					Ss = data.getSeconds() + ":";
+  					Ss = data.getSeconds();
 					var dataBeacon = gg + mm + aaaa + " "+ Hh + Mm + Ss;
-					/* Inserisco notizie nella tabella notifche per Beacon Azzurro */
+					/* Inserisco notizie nella tabella notifche per Beacon Azzurro 
 					 db = window.openDatabase("DatabaseSqlliteApp", "1.0", "Database prova", 200000);
                        db.transaction(
                             // Metodo di chiamata asincrona
@@ -189,7 +189,7 @@ app.runScanTimer = function()
                              function()  {
                                             alert("Inserimento effettuato");
                                          }
-                    )
+                    )*/
 					//navigator.notification.confirm('Notizia', onConfirm,'Beacon Azzurro',['Guarda','Salva']);
 				}
 				if(countDue==0 && uuid.toUpperCase()=="937BD9F3-5C44-971C-F389-35152A80C632")
