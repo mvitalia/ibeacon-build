37d38810ea1c8d3d6ed88faa61833a7354638f3a
mvitalia <p.guglielmo@mvitalia.com>
2016-11-30 10:55:47 +0100

    a

--- app.js
+++ app.js
@@ -210,6 +210,25 @@ app.runScanTimer = function()
                         +"<p style='font-size:10px; text-align:left !important;'>Descrizione: "+dati.rows.item(i).descrizione+"</p></a></li>"*/
             
             }
+						  				  for (var i in regions)
+		{
+			alert("Partenza regions");
+			alert(regions[i].uuid);
+			var beaconRegion = new locationManager.BeaconRegion(
+				i + 1,
+				regions[i].uuid);
+
+			// Start ranging.
+			locationManager.startRangingBeaconsInRegion(beaconRegion)
+				.fail(console.error)
+				.done();
+
+			// Start monitoring.
+			// (Not used in this example, included as a reference.)
+			locationManager.startMonitoringForRegion(beaconRegion)
+				.fail(console.error)
+				.done();
+		}
 		
             
         }
@@ -261,25 +280,7 @@ app.runScanTimer = function()
                     });
                       selezionaBeacon ();
 					  alert("Dopo beacon Forse");
-					  				  for (var i in regions)
-		{
-			alert("Partenza regions");
-			alert(regions[i].uuid);
-			var beaconRegion = new locationManager.BeaconRegion(
-				i + 1,
-				regions[i].uuid);
-
-			// Start ranging.
-			locationManager.startRangingBeaconsInRegion(beaconRegion)
-				.fail(console.error)
-				.done();
-
-			// Start monitoring.
-			// (Not used in this example, included as a reference.)
-			locationManager.startMonitoringForRegion(beaconRegion)
-				.fail(console.error)
-				.done();
-		}
+		
 		              
                 });
 		  }else{
