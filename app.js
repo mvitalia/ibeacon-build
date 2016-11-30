7697c4a852a915fe8f2fd66ce3ea1ad1ce35144a
mvitalia <p.guglielmo@mvitalia.com>
2016-11-30 09:33:57 +0100

    a

--- app.js
+++ app.js
@@ -19,17 +19,16 @@ var app = (function()
 	// Tabella dei beacon da rilevare 
 
 	// Specify your beacon 128bit UUIDs here.
-	var regions =
+	/*var regions =
 	[
 		// Estimote Beacon factory UUID.
 		{uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D'},//blu
 		// Sample UUIDs for beacons in our lab.
 		{uuid:'5F4DF8FB-3EC2-60B1-DB6F-6E7013122EE0'}, //azzurro
 		{uuid:'937BD9F3-5C44-971C-F389-35152A80C632'},	// verde
-	];
-	/* Per Beacon in dinamico 
+	];*/
+	/* Per Beacon in dinamico */ 
     var regions = [];
-	*/
 	// Background detection.
 	var notificationID = 0;
 	var inBackground = false;
