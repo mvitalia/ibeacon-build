c62463200787420ab27062a1f1d5315b6ae7579b
mvitalia <p.guglielmo@mvitalia.com>
2016-12-01 09:29:21 +0100

    a

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ var app = (function()
                             // Metodo di chiamata asincrona
                             function(tx) {
 								             //  tx.executeSql("DROP TABLE IF EXISTS letture");
-								             //tx.executeSql("DROP TABLE IF EXISTS notifiche");
+								               tx.executeSql("DROP TABLE IF EXISTS notifiche");
                                                tx.executeSql("CREATE TABLE IF NOT EXISTS letture (id INTEGER PRIMARY KEY AUTOINCREMENT,uuid, major, minor, data_ora, proximity, data_ora_lettura, nome_beacon)");
 									           tx.executeSql("CREATE TABLE IF NOT EXISTS notifiche (id INTEGER PRIMARY KEY AUTOINCREMENT,uuid, data_ora datetime, titolo, descrizione, immagine, link, allegato, attivo_da, attivo_a, ID_dispositivo, ID_notizia)");
                                           },
