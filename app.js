423c7881adc72af3a3688400b69f641d8ca2fadb
mvitalia <p.guglielmo@mvitalia.com>
2016-12-01 11:18:40 +0100

    a

--- app.js
+++ app.js
@@ -357,7 +357,7 @@ function startScan()
 										function()  {
 													//  alert("Inserimento effettuato Beacon Uno");
 													   sessionStorage.setItem('Id_notifica', ID_notizia);
-													   $( ":mobile-pagecontainer" ).pagecontainer( "change", "#dettaglioNotifica", {    transition: "flip" } );
+													   $( ":mobile-pagecontainer" ).pagecontainer( "change", "#dettaglioNotifica", {    transition: "flip",  reloadPage: true } );
 
 													 /*  $.mobile.changePage( "#dettaglioNotifica", {
 														    transition: "flip",
