4b92eea08d5a45c7b3bd8dfb50d26a64b97cf7d1
mvitalia <p.guglielmo@mvitalia.com>
2016-12-02 11:19:24 +0100

    a

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ var app = (function()
     
 	// Inizializzo matrico
 
-	var matrice_notizie = [[0],[0]];
+	//var matrice_notizie = [[0],[0]];
+	var  matrice_notizie  = new Array();
     var trovato = false;
 	// I beacon da rilevare in modo statico 
 	/*var regions =
@@ -325,21 +326,7 @@ function startScan()
 				var rilevaBeacon = false;
 
 				// controllo la dimensione della matrice
-			//	var matrice_lenght = matrice_notizie.length;
-			/*	if(matrice_lenght > 0){
-					matrice_notizie[0][0] = ID_dispositivo;
-					matrice_notizie[0][1] = ID_notizia;
-					
-				//	alert(matrice_lenght + ' - disp:'+ ID_dispositivo + ' - ' + matrice_notizie[0][0] + ' - ' + matrice_notizie[0][1]);
-				} else {
-					// matrice è vuota inserisco i valori appena rilevati
-				/*	matrice_notizie[0][0] = ID_dispositivo;
-					matrice_notizie[0][1] = ID_notizia;*/
-				//}
-				//	alert(ID_dispositivo);
-			//	matrice_notizie[0][0] = ID_dispositivo;
-			//		matrice_notizie[0][1] = ID_notizia;
-			//	alert(matrice_lenght + ' - disp:'+ ID_dispositivo + ' - ' + matrice_notizie[0][0] + ' - ' + matrice_notizie[0][1]);
+			//	
 
 			  /*  for (x=0; x<matrice[0].length; x++)
 				{
@@ -645,7 +632,23 @@ function startScan()
 
     function checkNotizia(ID_dispositivo,ID_notizia)
 	{
-		alert(ID_dispositivo +"-"+ ID_notizia );
+		
+		var matrice_lenght = matrice_notizie.length;
+		alert(matrice_lenght);
+			/*	if(matrice_lenght > 0){
+					matrice_notizie[0][0] = ID_dispositivo;
+					matrice_notizie[0][1] = ID_notizia;
+					
+				//	alert(matrice_lenght + ' - disp:'+ ID_dispositivo + ' - ' + matrice_notizie[0][0] + ' - ' + matrice_notizie[0][1]);
+				} else {
+					// matrice è vuota inserisco i valori appena rilevati
+				/*	matrice_notizie[0][0] = ID_dispositivo;
+					matrice_notizie[0][1] = ID_notizia;*/
+				//}
+				//	alert(ID_dispositivo);
+			//	matrice_notizie[0][0] = ID_dispositivo;
+			//		matrice_notizie[0][1] = ID_notizia;
+			//	alert(matrice_lenght + ' - disp:'+ ID_dispositivo + ' - ' + matrice_notizie[0][0] + ' - ' + matrice_notizie[0][1]);
 	}
 
 	function onConfirm(buttonIndex) {
