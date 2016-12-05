1129966fa9c8ede26e9ed5634d560f23b8f5fdcf
mvitalia <p.guglielmo@mvitalia.com>
2016-12-05 15:30:11 +0100

    a

--- app.js
+++ app.js
@@ -358,7 +358,7 @@ function startScan()
 													 // visualizza(ID_notizia);
 													 //  navigator.notification.confirm("Data: "+date, onConfirm,'Notifica: '+titolo_n,['Guarda','Salva']);
 													 // $( ".popupNotifica" ).popup( "open");
-													 var div ="<div id='"+ID_notizia+"' data-itemNot='"+ID_notizia+"' class='popNotifica'>"+
+													 var div ="<div id='"+ID_notizia+"' data-itemid='"+ID_notizia+"' class='popNotifica'>"+
 													 "<h3>Data: "+date+"</h3>"+
 													 "<p>Notifica: "+titolo_n+"</p>"+
 													 "<button type='submit' class='ui-btn' id='ApriNotifica'>Apri</button>"+
