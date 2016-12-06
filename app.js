e0cf51332df696267d78cc86f1046f296ae8b201
mvitalia <p.guglielmo@mvitalia.com>
2016-12-06 09:40:04 +0100

    a

--- app.js
+++ app.js
@@ -290,7 +290,7 @@ function startScan()
 				// Queto if permette di idetificare il Beacon a seconda della distanza
 				uuid =  beacon.uuid;
 				idUUID =uuid.toUpperCase();
-				var ID_dispositivo, ID_notizia = 0;
+				var ID_dispositivo=0, ID_notizia = 0;
 				var titolo_n,descrizione,immagine_n,link_n,allegato_n,attivo_da_n,attivo_a_n,data_creazione_n;
 				var restituito=true;
 				// Parte per rilevare o non rilevare il Beacon, ovvero se è già stato rilevato ed ha già mostrato la notizia
