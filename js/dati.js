0f8d73845d097c5011e0030ef565ab9bbe916d02
mvitalia <p.guglielmo@mvitalia.com>
2016-12-07 18:26:32 +0100

    a

--- js/dati.js
+++ js/dati.js
@@ -183,21 +183,21 @@ function loginUtente(usernameLogin,passLogin)
 
 function caricaNotifica ()
 {
-   
+   alert("ok");
     db = window.openDatabase("DatabaseSqlliteApp", "1.0", "Database prova", 200000);
      db.transaction(selectNotifica,successoSelectNotifica);  
 }
 
 function selectNotifica(tx)
 {
-        
+        alert("select");       
        tx.executeSql("SELECT * FROM notizie WHERE ID = "+localStorage.getItem('Id_notifica')+"",[], successoSelectNotifica,erroreSelect);        
 }
 
 function successoSelectNotifica(tx,dati)
 {
     var len = dati.rows.length;
-  
+        alert(len);
         var li_dati="";
         if(len!=0)
         {
@@ -322,7 +322,7 @@ function apriNotifica(id)
 {
     var prova = id;
      localStorage.setItem('Id_notifica', prova);
-    alert( localStorage.getItem('Id_notifica'));
+    alert(localStorage.getItem('Id_notifica'));
     $('.'+id+'').hide();
     $( ":mobile-pagecontainer" ).pagecontainer( "change", "notifica.html", {    transition: "flip", reload:true } );
 }
