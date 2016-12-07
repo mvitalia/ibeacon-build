120082d2216c0003a3b44fcc6407c402fafb2684
mvitalia <p.guglielmo@mvitalia.com>
2016-12-07 15:52:38 +0100

    a

--- app.js
+++ app.js
@@ -381,7 +381,7 @@ function startScan()
 															//alert(inBackground);
 														   	var div ="<div class='popNotifica "+ID_notizia+"' data-itemid='"+ID_notizia+"' >"+
 															"<h3>Data: "+date+"</h3>"+
-															"<p>Notifica: "+titolo_n+"-Background"+inBackground+"</p>"+
+															"<p>Notifica: "+titolo_n+"</p>"+
 															"<button  class='ui-btn' id='ApriNotifica'>Apri</button>"+
 															"<button  class='ui-btn' id='SalvaNotifica' onclick='salva_notifica(this," + ID_notizia + ")'>Salva</button>"+
 															"</div>";	
