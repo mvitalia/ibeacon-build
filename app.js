826ff3ed8e1ce2b421d33b1b8fe2f500fd3cc664
mvitalia <p.guglielmo@mvitalia.com>
2016-12-13 15:48:13 +0100

    a

--- app.js
+++ app.js
@@ -244,12 +244,14 @@ function startScan()
 		  // Creazione della tabella Beacon e notifiche se c'è o non c'è internet 
 		  var connessione = checkInternet();
 		  if(connessione==true){
-			  	window.FirebasePlugin.getToken(function(token) {
-    // save this server-side and use it to push notifications to this device
-    alert("Token"+token);
-}, function(error) {
-    alert("Errore"+error);
-});
+		FCMPlugin.getToken(
+  function(token){
+    alert(token);
+  },
+  function(err){
+    alert('error retrieving token: ' + err);
+  }
+)
 	     	//alert("internet"+connessione);
               // Creazione delle tabelle del db 
          		db = window.openDatabase("DatabaseSqlliteApp", "1.0", "Database prova", 200000);
